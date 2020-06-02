af90db9bf4f018a9d314f5510b35bc91ea4d74c1
Matthew Lister <mdlister24@hotmail.com>
2020-06-02 16:41:51 +0000

    Adding products url info

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def all_products(request):
+    """ A view to return all products, including sorting and search enquires """
+    return render(request, 'products/products.html', context)
